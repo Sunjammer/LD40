b5cebb9990e3d67d89587f3a53d9544ff5baa270
Vegard Strand Lende <lende.vegard@gmail.com>
2017-12-02 23:05:52 +0100

    Finish Kurskal

--- Source/coldBoot/map/Kurskal.hx
+++ Source/coldBoot/map/Kurskal.hx
@@ -13,16 +13,18 @@ import coldBoot.map.MapGenerator.TileMap;
 
 class Node implements Tile {
 
-    private var north:Bool;
-    private var east:Bool;
-    private var south:Bool;
-    private var west:Bool;
+    public var north:Bool;
+    public var east:Bool;
+    public var south:Bool;
+    public var west:Bool;
+    public var position:Vec2;
 
-    public function new() {
+    public function new(position: Vec2) {
         north = false;
         east = false;
         south = false;
         west = false;
+        this.position = position;
     }
 
     public function isOpen(): Bool {
@@ -45,6 +47,15 @@ class Node implements Tile {
             case Direction.South: south = true;
             case Direction.West: west = true;
         }
+    }
+
+    public function getSiblings(): Array<Vec2> {
+        var siblings = new Array<Vec2>();
+        if (north) siblings.push(position + DirectionExtensions.toVec2(Direction.North));
+        if (east) siblings.push(position + DirectionExtensions.toVec2(Direction.East));
+        if (south) siblings.push(position + DirectionExtensions.toVec2(Direction.South));
+        if (west) siblings.push(position + DirectionExtensions.toVec2(Direction.West));
+        return siblings;
     }
 }
 
@@ -95,18 +106,30 @@ class Kurskal implements TileMap {
     private var width: UInt;
     private var height: UInt;
 
-    private function new(width: UInt, height: UInt, nodes: Array<Node>) {
-        this.width = width;
-        this.height = height;
-        this.nodes = nodes;
-    }
-
     public function getTile(position: Vec2): Tile {
-        return nodes[offset(position, width, height)];
+        return nodes[offset(position)];
     }
 
     public function getBitmap(): Bitmap {
-        return null;
+        var bitmapData = new BitmapData(width * 3, height * 3, false, 0xff000000);
+        for (n in nodes) {
+            var p1 = new Point(n.position.x * 3 + 1, n.position.y * 3 + 1);
+            for (s in n.getSiblings()) {
+                var p2 = new Point(s.x * 3 + 1, s.y * 3 + 1);
+                drawLine(p1, p2, bitmapData);
+            }
+        }
+        return new Bitmap(bitmapData);
+    }
+    
+    private function drawLine(p1: Point, p2: Point, bitmapData: BitmapData) {
+        var len = Math.ceil(Point.distance(p1, p2));
+        for (i in 0...len) {
+            var p = Point.interpolate(p1, p2, i / len);
+            var x = Math.floor(Math.max(Math.min(p.x, width * 3 - 1), 0));
+            var y = Math.floor(Math.max(Math.min(p.y, height * 3 - 1), 0));
+            bitmapData.setPixel(x, y, 0xffffffff);
+        }
     }
 
     public function getWidth(): UInt {
@@ -118,39 +141,69 @@ class Kurskal implements TileMap {
     }
 
     public function getMap(): Array<Int> {
-        return null;
+        var size = (width * 3) * (height * 3);
+        var map = [for (i in 0...size) 1];
+        for (n in nodes) {
+            var p1 = new Point(n.position.x * 3 + 1, n.position.y * 3 + 1);
+            for (s in n.getSiblings()) {
+                var p2 = new Point(s.x * 3 + 1, s.y * 3 + 1);
+                plotLine(p1, p2, map);
+            }
+        }
+        return map;
     }
 
-    private static function offset(coord: Vec2, width: UInt, height: UInt): UInt {
+    private function plotLine(p1: Point, p2: Point, map: Array<Int>) {
+        var len = Math.ceil(Point.distance(p1, p2));
+        for (i in 0...len) {
+            var p = Point.interpolate(p1, p2, i / len);
+            var x = Math.floor(Math.max(Math.min(p.x, width * 3 - 1), 0));
+            var y = Math.floor(Math.max(Math.min(p.y, height * 3 - 1), 0));
+            var offset = (y * width * 3) + x;
+            map[offset] = 0;
+        }
+    }
+
+    private function offset(coord: Vec2): UInt {
         var x = Math.floor(coord.x);
         var y = Math.floor(coord.y);
         return y * height + x;
     }
 
-    public static function generate(randomSeed: Int, width: UInt, height: UInt): TileMap {
+    private function new(randomSeed: Int, width: UInt, height: UInt) {
+        this.width = width;
+        this.height = height;
         var edges = Randomize.shuffle(Edge.generateEdges(width, height));
         var tileCount = width * height;
-        var tiles = [for(i in 0...tileCount) new Node()];
+        nodes = new Array<Node>();
+        for (y in 0...height) {
+            for (x in 0...width) {
+                nodes.push(new Node(new Vec2(x, y)));
+            }
+        }
         var sets = [for(i in 0...tileCount) new Tree()];
 
         while (edges.length > 0) {
             var edge = edges.pop();   
             var newCoord = edge.coord + DirectionExtensions.toVec2(edge.dir);
-            var offset1 = offset(edge.coord, width, height);
-            var offset2 = offset(newCoord, width, height);
+            var offset1 = offset(edge.coord);
+            var offset2 = offset(newCoord);
             var set1 = sets[offset1];
             var set2 = sets[offset2];
 
             if (!set1.isConnected(set2)) {
                 set1.connect(set2);
 
-                var tile1 = tiles[offset1];
-                var tile2 = tiles[offset2];
+                var tile1 = nodes[offset1];
+                var tile2 = nodes[offset2];
 
                 tile1.setPathOpen(edge.dir);
                 tile2.setPathOpen(DirectionExtensions.getOppositeDirection(edge.dir));
             }
         }
-        return new Kurskal(width, height, tiles);
+    }
+
+    public static function generate(randomSeed: Int, width: UInt, height: UInt): TileMap {
+        return new Kurskal(randomSeed, width, height);
     }
 }
