9c88b9f790ffb9c25ba5c04e6cfc8eb73bfce18b
Emil Sandstø <emilalexer@hotmail.com>
2017-12-02 18:59:54 +0100

    Add explorer feature to enemy AI

---
+++ Source/EnemyAI.hx
@@ -0,0 +1,113 @@
+package;
+
+import lime.math.Vector2;
+import haxe.ds.GenericStack;
+
+interface Goal {
+}
+
+interface AtomicGoal extends Goal {
+
+}
+
+interface CompositeGoal extends Goal {
+
+}
+
+class PathMemory {
+	var nodesVisited:Map<Int,Bool>;
+
+	public function new() {
+	}
+
+	public function hasBeenHere(nodeIdx:Int) {
+		return nodesVisited.exists(nodeIdx);
+	}
+}
+
+class EnemyController {
+	public var position = new Vector2(0,0);
+	var moveSpeed:Float = 10;
+
+	public function move(dir:Vector2) {
+		 position.x += dir.x * moveSpeed;
+		 position.y += dir.y * moveSpeed;
+	}
+}
+
+class Enemy {
+	var pathMemory:PathMemory;
+	var controller:EnemyController;
+	var map:PathFinding.GameMap;
+	var enemyMap:PathFinding.GameMap;
+	
+	var currentPathStack:GenericStack<Int> = new GenericStack<Int>();
+	var currentPath:Array<Vector2>;
+	var knownWalkablePlaces:Array<Bool>;
+
+	public function new(map:PathFinding.GameMap, controller:EnemyController) {
+		this.controller = controller;
+		pathMemory = new PathMemory();
+		this.map = map;
+		knownWalkablePlaces = new Array<Bool>();
+		for(i in 0...map.cols*map.rows) {
+			knownWalkablePlaces[i] = false;
+		}
+		knownWalkablePlaces[map.toNodeIdx(controller.position)] = true; // Add spawn point
+
+		enemyMap = new PathFinding.GameMap(map.cols, map.rows, map.cellSize, knownWalkablePlaces);
+	}
+
+	public function performAction() {
+		if(currentPath.length == 0)
+		{
+			var currentIdx = map.toNodeIdx(controller.position);
+			var forwardIdx = map.toNodeIdx(controller.position) + map.cols;
+			var backwardIdx = map.toNodeIdx(controller.position) - map.cols;
+			var leftIdx = map.toNodeIdx(controller.position) - 1;
+			var rightIdx = map.toNodeIdx(controller.position) + 1;
+
+			if(map.isWalkableIdx(forwardIdx) && !knownWalkablePlaces[forwardIdx])
+			{
+				currentPathStack.add(forwardIdx);
+				knownWalkablePlaces[forwardIdx] = true;
+			}
+
+			if(map.isWalkableIdx(backwardIdx) && !knownWalkablePlaces[backwardIdx])
+			{
+				currentPathStack.add(backwardIdx);
+				knownWalkablePlaces[backwardIdx] = true;
+			}
+
+			if(map.isWalkableIdx(leftIdx) && !knownWalkablePlaces[leftIdx])
+			{
+				currentPathStack.add(leftIdx);
+				knownWalkablePlaces[leftIdx] = true;
+			}
+
+			if(map.isWalkableIdx(rightIdx) && !knownWalkablePlaces[rightIdx])
+			{
+				currentPathStack.add(rightIdx);
+				knownWalkablePlaces[rightIdx] = true;
+			}
+
+			var nextPath = currentPathStack.pop();
+			if(nextPath != null) {
+				currentPath = new PathFinding().ShortestPath(currentIdx, cast(nextPath, Int), this.enemyMap);
+				currentPath.reverse();
+			}
+		}
+
+		if(currentPath.length == 0)
+			neko.Lib.print("I'm stuck!");
+
+		var supposedToBeHere = currentPath[currentPath.length - 1];
+		if(Vector2.distance(supposedToBeHere, controller.position) < 10) {
+			currentPath.pop();
+		}
+
+		var dir = supposedToBeHere.subtract(controller.position);
+		dir.normalize(1);
+		controller.move(dir);
+	}
+}
---
+++ Source/PathFinding.hx
@@ -0,0 +1,60 @@
+package;
+
+import pathfinder.*;
+import lime.math.Vector2;
+
+class GameMap implements IMap {	
+	public var rows( default, null ):Int;
+	public var cols( default, null ):Int;
+
+	public var cellSize:Float;
+	var isWalkableData:Array<Bool>;
+
+	public function new(p_cols:Int, p_rows:Int, cellSize:Float, isWalkableData:Array<Bool>)
+	{
+		cols = p_cols;
+		rows = p_rows;
+		this.cellSize = cellSize;
+		this.isWalkableData = isWalkableData;
+		// create an array of tiles, and determine if they are walkable or obstructed
+	}
+	
+	public function isWalkable(p_x:Int, p_y:Int):Bool
+	{
+		return isWalkableIdx(posToNodeIdx(p_x, p_y));
+	}
+
+	public function isWalkableIdx(nodeIdx:Int):Bool
+	{
+		return this.isWalkableData[nodeIdx];
+	}
+
+	public function toNodeIdx(pos:Vector2):Int {
+		return cast(pos.x/cellSize,Int) % cols
+			+ cast((pos.y/cellSize) * cols, Int);
+	}
+
+	function posToNodeIdx(x:Int, y:Int):Int {
+		return x % cols
+			+ cast(y * cols, Int);
+	}
+
+	public function toCoordinate(nodeIdx:Int):Coordinate {
+		return new Coordinate(nodeIdx % cols, cast(nodeIdx / cols, Int));
+	}
+}
+
+class PathFinding {
+	public function new() {
+
+	}
+
+	public function ShortestPath(sourceIdx:Int, toTargetIdx:Int, map:GameMap):Array<Vector2> {
+		var source = map.toCoordinate(sourceIdx);
+		var toTarget = map.toCoordinate(toTargetIdx);
+		var pathFinder = new Pathfinder(map);
+		return pathFinder.createPath(new Coordinate(cast(source.x, Int), cast(source.y, Int)), new Coordinate(cast(toTarget.x, Int), cast(toTarget.y, Int)), EHeuristic.PRODUCT, false, false).map(function(coord) {
+			return new Vector2(coord.x*map.cellSize+map.cellSize/2, coord.y*map.cellSize+map.cellSize/2);
+		});
+	}
+}
