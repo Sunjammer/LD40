6435bcaba2f46db8eb6f77547e1f8834ab7a16d7
Tapped <emilalexer@hotmail.com>
2017-12-03 00:45:49 +0100

    Fix mac rendering

--- Source/coldBoot/rendering/PostEffect.hx
+++ Source/coldBoot/rendering/PostEffect.hx
@@ -37,11 +37,17 @@ class PostEffect
 	private var uniforms:Map<String, Uniform>;
 
 	private static inline var fullscreenQuadFrag:String = "
-    #version 330 core
-    const vec4 quadVertices[4] = { vec4( -1.0, -1.0, 0.0, 0.0), vec4(1.0, -1.0, 1.0, 0.0), vec4( -1.0, 1.0, 0.0, 1.0), vec4(1.0, 1.0, 1.0, 1.0) };
+    #version 120
+	#extension GL_EXT_gpu_shader4 : require
     varying vec2 vTexCoord;
     void main()
     {
+		vec4 quadVertices[4];
+		quadVertices[0] = vec4( -1.0, -1.0, 0.0, 0.0); 
+		quadVertices[1] = vec4(1.0, -1.0, 1.0, 0.0); 
+		quadVertices[2] = vec4( -1.0, 1.0, 0.0, 1.0); 
+		quadVertices[3] = vec4(1.0, 1.0, 1.0, 1.0);
+
         vTexCoord = quadVertices[gl_VertexID].zw;
         gl_Position = vec4(quadVertices[gl_VertexID].xy, 0.0, 1.0);
     }
