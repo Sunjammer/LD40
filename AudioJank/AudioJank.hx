63e26e7aa814c0a99fb1c99f08df6832e9c85ed2
Andreas Kennedy <andreas@doomsday.no>
2017-12-02 18:44:25 +0100

    AudioJank extension project

---
+++ AudioJank/AudioJank.hx
@@ -0,0 +1,18 @@
+package;
+
+
+import lime.system.CFFI;
+import lime.system.JNI;
+
+
+class AudioJank {
+	
+	
+	public static function sampleMethod (inputValue:Int):Int {
+		return audiojank_sample_method(inputValue);
+	}
+	
+	
+	private static var audiojank_sample_method = CFFI.load ("audiojank", "audiojank_sample_method", 1);
+	
+}
