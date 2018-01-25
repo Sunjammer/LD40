9dba08b07e15532c26e06a5ddf3a4940fc270494
Andreas Rønning <andreas@doomsday.no>
2018-01-25 18:52:15 +0100

    Fix audiojank build on windows

--- build_audio_shiz.bat
+++ build_audio_shiz.bat
@@ -1,3 +1,3 @@
-cd AudioJank/project/audio_lib && cargo build release && cd ../../..
+cd AudioJank/project/audio_lib && cargo build --release && cd ../../..
 openfl rebuild hxcpp windows
 openfl rebuild AudioJank windows
